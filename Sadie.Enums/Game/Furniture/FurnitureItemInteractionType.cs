2729521f1b43cc81cf3598d1873cd1daf3e1d45c
ashdevelops <ash.smith.dev@gmail.com>
2025-02-21 19:14:29 +0000

    Fixed stuff

--- Sadie.Enums/Game/Furniture/FurnitureItemInteractionType.cs
+++ Sadie.Enums/Game/Furniture/FurnitureItemInteractionType.cs
@@ -1,4 +1,4 @@
-namespace Sadie.Enums.Game.Furniture;
+namespace Sadie.Enums.Game.Furniture,
 
 public static class FurnitureItemInteractionType
 {
@@ -17,10 +17,10 @@ public static class FurnitureItemInteractionType
         WiredTriggerFurnitureStateChanged = "wf_trg_state_changed",
         WiredTriggerEnterRoom = "wf_trg_enter_room",
         WiredEffectShowMessage = "wf_act_show_message",
-        WiredEffectKickUser = "wf_act_kick_user";
-        WiredEffectTeleportToFurniture = "wf_act_teleport_to";
-        WiredEffectMoveRotateFurniture = "wf_act_move_rotate";
-        WiredEffectToggleFurnitureState = "wf_act_toggle_state";
-        WiredEffectMoveFurnitureToClosestUser = "wf_act_chase";
-        WiredEffectMuteTriggerer = "wf_act_mute_triggerer";
+        WiredEffectKickUser = "wf_act_kick_user",
+        WiredEffectTeleportToFurniture = "wf_act_teleport_to",
+        WiredEffectMoveRotateFurniture = "wf_act_move_rotate",
+        WiredEffectToggleFurnitureState = "wf_act_toggle_state",
+        WiredEffectMoveFurnitureToClosestUser = "wf_act_chase",
+        WiredEffectMuteTriggerer = "wf_act_mute_triggerer"
 }
