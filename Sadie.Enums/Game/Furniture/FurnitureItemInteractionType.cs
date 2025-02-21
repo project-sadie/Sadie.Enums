d7934f7ed370858f7895647e0cb5415fd3befe2f
ashdevelops <ash.smith.dev@gmail.com>
2025-02-21 19:11:23 +0000

    Adding interaction types

--- Sadie.Enums/Game/Furniture/FurnitureItemInteractionType.cs
+++ Sadie.Enums/Game/Furniture/FurnitureItemInteractionType.cs
@@ -18,4 +18,9 @@ public static class FurnitureItemInteractionType
         WiredTriggerEnterRoom = "wf_trg_enter_room",
         WiredEffectShowMessage = "wf_act_show_message",
         WiredEffectKickUser = "wf_act_kick_user";
+        WiredEffectTeleportToFurniture = "wf_act_teleport_to";
+        WiredEffectMoveRotateFurniture = "wf_act_move_rotate";
+        WiredEffectToggleFurnitureState = "wf_act_toggle_state";
+        WiredEffectMoveFurnitureToClosestUser = "wf_act_chase";
+        WiredEffectMuteTriggerer = "wf_act_mute_triggerer";
 }
