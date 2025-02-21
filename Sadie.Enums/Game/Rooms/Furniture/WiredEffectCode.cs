6791d83b385770a8177cddbf1a2ed733492783be
ashdevelops <ash.smith.dev@gmail.com>
2025-02-21 19:40:35 +0000

    new wired effect codes

--- Sadie.Enums/Game/Rooms/Furniture/WiredEffectCode.cs
+++ Sadie.Enums/Game/Rooms/Furniture/WiredEffectCode.cs
@@ -2,6 +2,11 @@ namespace Sadie.Enums.Game.Rooms.Furniture;
 
 public enum WiredEffectCode
 {
+    TeleportToFurniture = 8,
     ShowMessage = 7,
     KickUser = 19,
+    MoveFurnitureToClosestUser = 11,
+    MuteTriggerer = 20,
+    BotChangedClothes = 26,
+    BotTalkToAvatar = 27
 }
